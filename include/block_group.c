724e91e1efb3d4a543cd29ada737bf0689ccbb8a
Andre F Baretta <andrefbaretta@gmail.com>
2025-05-29 18:45:29 -0300

    Add headers

--- include/block_group.c
+++ include/block_group.c
@@ -1,6 +1,10 @@
-#include <stdint.h>
+// - Descrição:............ Struct de block group
+// - Autor:................ André Felipe Baretta
+// - Data de Criação:...... 26/05/2025
+// - Data de Atualização:.. 26/05/2025
 
+#include <stdint.h>
 
 typedef struct {
    
-}
+} block_group_descriptor;
