0732c8cc0dfa3e1168fa2f8bafc661992fd6e5bb
joao-pedro-araujo <jpia957@gmail.com>
2025-05-29 19:27:59 -0300

    alterando projeto

--- include/EXT2.h
+++ include/EXT2.h
@@ -1,9 +1,9 @@
-// - Descrição:............ Struct de Superblock
-// - Autor:................ Henry Meneguini Farias
-// - Data de Criação:...... 25/05/2025
-// - Data de Atualização:.. 26/05/2025
-
 #include <stdint.h>
+
+//Estruturas de dados do sistesma de arquivos EXT2
+
+//***SUPERBLOCK:
+
 #pragma pack(push,1)
 
 typedef struct Superblock{
@@ -21,7 +21,7 @@ typedef struct Superblock{
    uint32_t s_wtime;
    uint16_t s_mnt_count;
    uint16_t s_max_mnt_count;
-   uint16_t s_magic
+   uint16_t s_magic;
    uint16_t s_state;
    uint16_t s_errors;
    uint16_t s_minor_rev_level;
@@ -56,5 +56,43 @@ typedef struct Superblock{
    char s_unused[760];
 
 } Superblock;
+
+//***BLOCK GROUP DESCRIPTOR:
+
+typedef struct {
+   
+} block_group_descriptor;
+
+//***I_OSD2:
+typedef struct {
+   char h_i_frag;
+   char h_i_fsize;
+   uint16_t h_i_mode_high;
+   uint16_t h_i_uid_high;
+   uint16_t h_i_gid_high;
+   uint32_t h_i_author;
+} i_osd2;
+
+//***INODE:
+typedef struct {
+   uint16_t i_mode;
+   uint16_t i_uid;
+   uint32_t i_size;
+   uint32_t i_atime;
+   uint32_t i_ctime;
+   uint32_t i_mtime;
+   uint32_t i_dtime;
+   uint16_t i_gid;
+   uint16_t i_links_count;
+   uint32_t i_blocks;
+   uint32_t i_flags;
+   uint32_t i_ctime;
+   uint32_t i_block[15];
+   uint32_t i_generation;
+   uint32_t i_file_acl;
+   uint32_t i_dir_acl;
+   uint32_t i_faddr;
+   i_osd2 i_osd2;
+} Inode;
 
 #pragma pack(pop)
---
+++ src/EXT2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "EXT2.h"
+
+//Operações de leitura e escrita no sistema de arquivos EXT2:
+// int read_superblock(FILE *file, Superblock *sb);
+// int write_superblock(FILE *file, Superblock *sb);
+// int read_block_group_descriptor(FILE *file, BlockGroupDescriptor *bgd, int group_number);
+// int write_block_group_descriptor(FILE *file, BlockGroupDescriptor *bgd, int group_number);
+// int read_inode(FILE *file, Inode *inode, int inode_number);
+// int write_inode(FILE *file, Inode *inode, int inode_number);
+// int read_block(FILE *file, void *buffer, int block_number);
+// int write_block(FILE *file, const void *buffer, int block_number);
+// int read_inode_bitmap(FILE *file, uint8_t *bitmap, int group_number);
+// int write_inode_bitmap(FILE *file, const uint8_t *bitmap, int group_number);
+// int read_block_bitmap(FILE *file, uint8_t *bitmap, int group_number);
+// int write_block_bitmap(FILE *file, const uint8_t *bitmap, int group_number);
+// int read_directory_entry(FILE *file, DirectoryEntry *entry, int inode_number, int offset);
+// int write_directory_entry(FILE *file, const DirectoryEntry *entry, int inode_number, int offset);
+// int read_file_data(FILE *file, void *buffer, int inode_number, int offset, size_t size);
+// int write_file_data(FILE *file, const void *buffer, int inode_number, int offset, size_t size);
+    
